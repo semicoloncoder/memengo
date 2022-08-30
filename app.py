3cc822f9ca30493dc7b3b90f51bbcae4b35cb123
praveen-tamil <67270359+praveen-tamil@users.noreply.github.com>
2022-08-30 23:51:24 +0530

    Initial setup

--- app.py
+++ app.py
@@ -1,8 +1,52 @@
-from flask import Flask
-import json, requests
+from flask import Flask, render_template
+import json, requests, os
+from typing import Final
 
-app = Flask(__name__)
+APP_NAME: Final = 'Memengo'
 
+app = Flask(__name__, static_folder = 'assets')
+
+# getAMeme
+def getAMeme():
+    memeInfo = None
+
+    try:
+        response = requests.get('https://meme-api.herokuapp.com/gimme/wholesomememes')
+
+        if response and response.ok and response.status_code == 200:
+            jsonResponse = response.json()
+
+            if jsonResponse and type(jsonResponse) is dict and 'preview' in jsonResponse:
+                memeFileName = os.path.basename(jsonResponse['url'])
+
+                memeInfo = {
+                    'dispURL': jsonResponse['preview'][-2],
+                    'postTitle': jsonResponse['title'],
+                    'postLink': jsonResponse['postLink'],
+                    'postSubReddit': jsonResponse['subreddit'],
+                    'postAuthor': jsonResponse['author'],
+                    'downloadLink': jsonResponse['preview'][-1],
+                }
+
+                if memeFileName:
+                    memeInfo['memeFileName'] = memeFileName
+    except Exception as e:
+        memeInfo = None
+
+    return memeInfo
+# getAMeme
+
+# indexPage
 @app.route('/')
 def indexPage():
-    return 'Welcome to memengo app.'
+    memeInfo = getAMeme()
+
+    return render_template(
+        'indexPage.template.html',
+        appName = APP_NAME,
+        memeInfo = memeInfo,
+    )
+# indexPage
+
+# REMOVE BELOW WHEN DEPLOY
+app.run('0.0.0.0', port = 1463)
